816a833eb52035bf5c9aefc30ffac589eb9b64fe
Jordi Neil Sanchez <jordsanc@bancolombia.com.co>
2020-06-30 19:24:15 -0500

    Se agrega el ejemplo de TDD

---
+++ TDD/programasJava/src/com/company/Calculadora.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Calculadora {
+
+    public int valor = 0;
+
+    public int Valor() {
+        return 1;
+    }
+
+    /*public void Sumar(int sumando1, int sumando2) {
+        valor = sumando1 + sumando2;
+        //return 0;
+    }*/
+
+    /*public void Multiplicar(int multiplicando1, int multiplicando2) {
+        valor = multiplicando1*multiplicando2;
+    }*/
+}
